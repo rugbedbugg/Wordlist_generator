057bb2550d5efa4cfa9740b25b269175ce6e5626
oxide 1-6 <160270614+rugbedbugg@users.noreply.github.com>
2025-02-24 13:49:57 +0530

    Created version1

--- uname_pass_conc-v1.py
+++ uname_pass_conc-v1.py
@@ -1,15 +1,36 @@
-print(r"""
-======================================================================================================================================
+RED = "\033[1;31m"
+YELLOW = "\033[1;93m"
+PURPLE = "\033[35m"
+RESET = "\033[0m"
 
+
+print(fr"""
+======================================================================================================================================
+{RED}
  __      __                .___.__  .__           _      ________                                   _
 /  \    /  \___________  __| _/|  | |__| ______ _/ |_   /  _____/  ____   ____   _______________  _/ |__  ____  _____
 \   \/\/   /  _ \_  __ \/ __ | |  | |  |/  ___/ |  __| /   \  ____/ __ \ /    \_/ __ \_  __ \__ \ |   __|/  _ \|  ___|
  \        (  <_> )  | \/ /_/ | |  |_|  |\___ \  |  |   \    \_\  \  ___/|   |  \  ___/|  | \// _ \|  |  (  <_> )  |
   \__/\  / \____/|__|  \____ | |____/__/____  > |__|    \______  /\___  >___|  /\___  >__|  (____/|__|   \____/|__|
-       \/                   \/              \/                 \/     \/     \/     \/          	      version 1
+       \/                   \/              \/                 \/     \/     \/     \/          	      {YELLOW}version 1{RESET}
 
-															by @rugbedbugg
+															by {PURPLE}@rugbedbugg{RESET}
 =======================================================================================================================================
+This is a wordlist generator that takes in the file names of the username and passowrd files.
+For best experience, try running this program in the same directory as the username and password files!!
+
+{RED}[IMPORTANT]{RESET}
+The output format of the resulting wordlist is
+{YELLOW}"username:password"{RESET}
+
+This is my first wordlist I've made, updates will be made in future! 💪 💪 💪
+Other output formats will be supported in subsequent versions 😉
+
+
+
+Use this tool at your own discretion!!!
+I will NOT be responsible for any illegal actions facilitated by this tool 💀💀💀
+
 """)
 
 uname_file = input("Enter username file: ")
@@ -46,3 +67,4 @@ with open(combined_file, "w") as final:
        		final.write(f"{usernames[i]}:{passwords[i]}\n")
 
 print(f"Successful created {combined_file} with {length} usernames and passwords")
+
